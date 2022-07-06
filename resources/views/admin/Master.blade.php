affc90b0bd891651f86b5084ef0dfcd7a9433270
trinhhung00 <trinhhung2110@gmail.com>
2022-07-07 01:00:18 +0700

    1

--- resources/views/admin/Master.blade.php
+++ resources/views/admin/Master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+  {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
   <title>{{__('Admin')}}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
