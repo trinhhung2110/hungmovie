642b2e15cf4d5901bd766f51b9c6cddcd768192d
trinhhung2110 <hung.tq@beetechsoft.vn>
2022-07-12 15:21:39 +0700

    1

--- resources/views/user/upgrade.blade.php
+++ resources/views/user/upgrade.blade.php
@@ -5,49 +5,59 @@
 <link rel="stylesheet" href="{{ asset('css/user/elegant-icons.css ')}}" type="text/css">
 <link rel="stylesheet" href="{{ asset('css/user/myCss/profile.css ')}}">
 <link rel="stylesheet" href="{{ asset('AdminLTE-3.0.5/AdminLTE-3.0.5/plugins/fontawesome-free/css/all.min.css')}}">
+<style>
+    .card-body {
+        width: 80%;
+    }
+    h5 {
+        color: white
+    }
+</style>
 @endsection
 
 @section('content')
 <section class="content">
-    <div class="container-fluid">
-        <div class="col-md-4 pt-4 px-md-2 px-lg-3">
-            <div class="card">
-              <div class="card-body d-flex flex-column justify-content-around mx-auto">
-                <div class="text-center pt-5">
-                  <h5 class="my-4">Week Plan</h5>
-                </div>
+    <div class="container mb-5">
+        <div class="row mb-5">
+            <div class="col-md-4 pt-4 px-md-2 px-lg-3">
+                <div class="card">
+                  <div class="card-body d-flex flex-column justify-content-around mx-auto">
+                    <div class="text-center pt-5">
+                      <h5 class="my-4">Week Plan</h5>
+                    </div>
 
-                <div class="text-center my-5">
-                  <h2 class="mb-3">Free</h2><button class="btn btn-outline-danger rounded-pill" type="submit">Sign Up </button>
+                    <div class="text-center my-5">
+                      <h2 class="mb-3">10.000vnd/week</h2><button class="btn btn-outline-danger rounded-pill" type="submit">Sign Up </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+            </div>
+            <div class="col-md-4 pt-4 px-md-2 px-lg-3">
+                <div class="card">
+                  <div class="card-body d-flex flex-column justify-content-around mx-auto">
+                    <div class="text-center pt-5">
+                      <h5 class="my-4">Month Plan</h5>
+                    </div>
+                    <div class="text-center my-5">
+                      <h2 class="mb-3">35.000vnd/month</h2><button class="btn btn-outline-danger rounded-pill" type="submit">Sign Up </button>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4 pt-4 px-md-2 px-lg-3">
+                <div class="card">
+                  <div class="card-body d-flex flex-column justify-content-around mx-auto">
+                    <div class="text-center pt-5">
+                      <h5 class="my-4">Year Plan</h5>
+                    </div>
+                    <div class="text-center my-5">
+                      <h2 class="mb-3">380.000vnd/year</h2><button class="btn btn-outline-danger rounded-pill" type="submit">Sign Up </button>
+                    </div>
+                  </div>
+                </div>
             </div>
         </div>
-        <div class="col-md-4 pt-4 px-md-2 px-lg-3">
-            <div class="card">
-              <div class="card-body d-flex flex-column justify-content-around mx-auto">
-                <div class="text-center pt-5">
-                  <h5 class="my-4">Month Plan</h5>
-                </div>
 
-                <div class="text-center my-5">
-                  <h2 class="mb-3">Free</h2><button class="btn btn-outline-danger rounded-pill" type="submit">Sign Up </button>
-                </div>
-              </div>
-            </div>
-        </div>
-        <div class="col-md-4 pt-4 px-md-2 px-lg-3">
-            <div class="card">
-              <div class="card-body d-flex flex-column justify-content-around mx-auto">
-                <div class="text-center pt-5">
-                  <h5 class="my-4">Year Plan</h5>
-                </div>
-                <div class="text-center my-5">
-                  <h2 class="mb-3">Free</h2><button class="btn btn-outline-danger rounded-pill" type="submit">Sign Up </button>
-                </div>
-              </div>
-            </div>
-        </div>
     </div>
 </section>
 @endsection
