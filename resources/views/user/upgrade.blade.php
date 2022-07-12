2d1212097562edf3188c29597c8b8ba4aead2585
trinhhung00 <trinhhung2110@gmail.com>
2022-07-12 21:26:28 +0700

    1

--- resources/views/user/upgrade.blade.php
+++ resources/views/user/upgrade.blade.php
@@ -23,62 +23,10 @@
 @endif
 <section class="content">
     <div class="container mb-5">
-        @if (auth()->user())
-            @if (auth()->user()->is_pay == 0 && auth()->user()->expired_at <= Carbon\Carbon::now())
-            <div class="row mb-5">
-                <div class="col-md-4 pt-4 px-md-2 px-lg-3">
-                    <div class="card">
-                    <div class="card-body d-flex flex-column justify-content-around mx-auto">
-                        <div class="text-center pt-5">
-                        <h5 class="my-4">Week Plan</h5>
-                        </div>
-                        <div class="text-center my-5">
-                        <h2 class="mb-3">20.000vnd/week</h2>
-                            <form action="{{ route('upgrade.commit', ['type' => 1]) }}" method="POST" target="_blank">
-                                @csrf
-                                <button type="submit" name="redirect" class="btn btn-outline-danger rounded-pill" >Upgrade </button>
-                            </form>
-
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div class="col-md-4 pt-4 px-md-2 px-lg-3">
-                    <div class="card">
-                    <div class="card-body d-flex flex-column justify-content-around mx-auto">
-                        <div class="text-center pt-5">
-                        <h5 class="my-4">Month Plan</h5>
-                        </div>
-                        <div class="text-center my-5">
-                        <h2 class="mb-3">70.000vnd/month</h2>
-                            <form action="{{ route('upgrade.commit', ['type' => 2]) }}" method="POST" target="_blank">
-                                @csrf
-                                <button type="submit" name="redirect" class="btn btn-outline-danger rounded-pill" >Upgrade </button>
-                            </form>              </div>
-                    </div>
-                    </div>
-                </div>
-                <div class="col-md-4 pt-4 px-md-2 px-lg-3">
-                    <div class="card">
-                    <div class="card-body d-flex flex-column justify-content-around mx-auto">
-                        <div class="text-center pt-5">
-                        <h5 class="my-4">Year Plan</h5>
-                        </div>
-                        <div class="text-center my-5">
-                        <h2 class="mb-3">750.000vnd/year</h2>
-                            <form action="{{ route('upgrade.commit', ['type' => 3]) }}" method="POST" target="_blank">
-                                @csrf
-                                <button type="submit" name="redirect" class="btn btn-outline-danger rounded-pill" >Upgrade </button>
-                            </form>             </div>
-                    </div>
-                    </div>
-                </div>
+        @if (auth()->user()->is_pay == 1 && auth()->user()->expired_at >= Carbon\Carbon::now())
+            <div class="alert alert-light text-center" role="alert">
+                Your account has been upgraded, the expired date is : {{ Carbon\Carbon::parse(auth()->user()->expired_at)->format('d/m/Y'); }}
             </div>
-            @else
-                <div class="alert alert-light" role="alert">
-                    Your account has been upgraded, the expiry date is : {{ Carbon\Carbon::parse(auth()->user()->expired_at)->format('d/m/Y'); }}
-                </div>
-            @endif
         @else
             <div class="row mb-5">
                 <div class="col-md-4 pt-4 px-md-2 px-lg-3">
@@ -87,10 +35,12 @@
                         <div class="text-center pt-5">
                         <h5 class="my-4">Week Plan</h5>
                         </div>
-
                         <div class="text-center my-5">
-                        <h2 class="mb-3">20.000vnd/week</h2>
-                            <a href="{{ route('user_login') }}" class="btn btn-outline-danger rounded-pill" >Login</a>
+                         <h2 class="mb-3">20.000vnd/week</h2>
+                            <form action="{{ route('upgrade.commit', ['type' => 1]) }}" method="POST">
+                                @csrf
+                                <button type="submit" name="redirect" class="btn btn-outline-danger rounded-pill" >Upgrade </button>
+                            </form>
                         </div>
                     </div>
                     </div>
@@ -102,9 +52,12 @@
                         <h5 class="my-4">Month Plan</h5>
                         </div>
                         <div class="text-center my-5">
-                        <h2 class="mb-3">70.000vnd/month</h2>
-                            <a href="{{ route('user_login') }}" class="btn btn-outline-danger rounded-pill" >Login</a>
-                  </div>
+                            <h2 class="mb-3">70.000vnd/month</h2>
+                            <form action="{{ route('upgrade.commit', ['type' => 2]) }}" method="POST">
+                                @csrf
+                                <button type="submit" name="redirect" class="btn btn-outline-danger rounded-pill" >Upgrade </button>
+                            </form>
+                        </div>
                     </div>
                     </div>
                 </div>
@@ -115,9 +68,12 @@
                         <h5 class="my-4">Year Plan</h5>
                         </div>
                         <div class="text-center my-5">
-                        <h2 class="mb-3">750.000vnd/year</h2>
-                            <a href="{{ route('user_login') }}" class="btn btn-outline-danger rounded-pill" >Login</a>
-                    </div>
+                            <h2 class="mb-3">750.000vnd/year</h2>
+                            <form action="{{ route('upgrade.commit', ['type' => 3]) }}" method="POST">
+                                @csrf
+                                <button type="submit" name="redirect" class="btn btn-outline-danger rounded-pill"> Upgrade </button>
+                            </form>
+                        </div>
                     </div>
                     </div>
                 </div>
